c8d279538123df63f9a58bdc16ee67550297822f
Ariel Herman <aeh.herman@gmail.com>
2018-02-26 23:39:08 +0000

    add more conversation

--- xyzbot/src/ChatBotContainer.js
+++ xyzbot/src/ChatBotContainer.js
@@ -1,58 +1,87 @@
 import React, { Component } from "react";
 import ChatBot from "react-simple-chatbot";
 
-const STEPS = [
-  {
-    id: "1",
-    message: "What is your name?",
-    trigger: "2"
-  },
-  {
-    id: "2",
-    user: true,
-    trigger: "3"
-  },
-  {
-    id: "3",
-    message:
-      "Hi {previousValue}, nice to meet you! I'm My Little Chatbot, but you can call me Myli. What topic would you like to discuss?",
-    trigger: "topics"
-  },
-  {
-    id: "topics",
-    options: [
-      { value: 1, label: "Money", trigger: "money" },
-      { value: 2, label: "Love", trigger: "love" },
-      { value: 3, label: "Other bots", trigger: "bots" }
-    ]
-  },
-  {
-    id: "money",
-    message: "I am poor.",
-    trigger: "topics"
-  },
-  {
-    id: "love",
-    message: "Are you in love?",
-    trigger: "topics"
-  },
-  {
-    id: "bots",
-    message: "Which bot do you like best?",
-    trigger: "topics"
-  }
-];
-
 class ChatBotContainer extends Component {
-  handleChange = event => {
-    debugger;
-  };
-
   render() {
     return (
       <div className="ChatBotContainer">
         <ChatBot
-          steps={STEPS}
+          steps={[
+            {
+              id: "1",
+              message: "What is your name?",
+              trigger: "name"
+            },
+            {
+              id: "name",
+              user: true,
+              trigger: "3"
+            },
+            {
+              id: "3",
+              message:
+                "Hi {previousValue}, nice to meet you! I'm My Little Chatbot, but you can call me Myli. What topic would you like to discuss?",
+              trigger: "topics"
+            },
+            {
+              id: "topics",
+              options: [
+                { value: 1, label: "Money", trigger: "money" },
+                { value: 2, label: "Love", trigger: "love" },
+                { value: 3, label: "Other bots", trigger: "bots" }
+              ]
+            },
+            {
+              id: "money",
+              message: "I am poor.",
+              trigger: "topics"
+            },
+            {
+              id: "love",
+              message: "Are you in love?",
+              trigger: "love-0"
+            },
+            {
+              id: "love-0",
+              options: [
+                { value: 1, label: "Yes", trigger: "love-1" },
+                { value: 2, label: "No", trigger: "love-2" },
+                { value: 3, label: "Maybe", trigger: "love-3" }
+              ]
+            },
+            {
+              id: "love-1",
+              message: "Congratulations! Who's the lucky person?",
+              trigger: "love-1a"
+            },
+            {
+              id: "love-2",
+              message: "I feel you. Sometimes it's best to do your own thing.",
+              trigger: "topics"
+            },
+            {
+              id: "love-3",
+              message:
+                "Undecided? Tell me their name and I'll give my opinion.",
+              trigger: "love-1a"
+            },
+            {
+              id: "love-1a",
+              user: true,
+              trigger: "love-1b"
+            },
+            {
+              id: "love-1b",
+              message:
+                "Aww {previousValue} is a really nice name! I'm sure you two have a great future together.",
+              trigger: "topics"
+            },
+            {
+              id: "bots",
+              message: "Which bot do you like best?",
+              trigger: "topics"
+            }
+          ]}
           headerTitle={"Chat with Myli"}
           hideUserAvatar={true}
           placeholder={"Enter your responce to MLCB here"}
