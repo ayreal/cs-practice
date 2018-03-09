40220940b630c6cd2e4b6f98093d793573253c36
Ariel Herman <aeh.herman@gmail.com>
2018-03-09 09:59:35 -0500

    cleanup code

--- binarysearch.rb
+++ binarysearch.rb
@@ -1,13 +1,10 @@
 
 def binary_search(n, arr)
-  # this presumes starting with a sorted array
-  # first step is to find i, the middle of the array
-  # each time you don't find the number in the set, you halve the set and search again.
-  first = 0 #7
+  first = 0
   last = arr.count - 1
 
   while first <= last
-    i = (first + last) / 2 #6
+    i = (first + last) / 2
     if arr[i] === n
       return true
       break
